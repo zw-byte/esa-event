1399abcdf04c565797a9d844635bae8f56865f38
OneDay AI 代码助手 <oneday_bot@alibaba-inc.com>
2025-11-06 15:40:19 +0800

    011zMNQW_mqxvosp_esa-campaign-page_更新InfoQ的logo @ 罗序禧

--- src/components/Rules.jsx
+++ src/components/Rules.jsx
@@ -139,7 +139,7 @@ function Rules() {
               { name: 'B站专栏', icon: 'fab fa-bilibili', url: 'https://www.bilibili.com/read/home' },
               { name: 'GitHub', icon: 'fab fa-github', url: 'https://github.com' },
               { name: 'SegmentFault', icon: 'fas fa-code', url: 'https://segmentfault.com' },
-              { name: 'InfoQ', icon: 'fas fa-newspaper', url: 'https://www.infoq.cn' },
+              { name: 'InfoQ', icon: 'https://1d-static.alibaba-inc.com/oneday/source/e656dcd9-0d86-47e3-872c-e957f5d9257e.PNG', url: 'https://www.infoq.cn', isImage: true },
               { name: 'Linux', icon: 'fab fa-linux', url: 'https://linux.cn' },
               { name: 'Golang中文社区', icon: 'fas fa-code-branch', url: 'https://studygolang.com' },
               { name: '博客园', icon: 'fas fa-leaf', url: 'https://www.cnblogs.com' },
